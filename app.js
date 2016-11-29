83b492257951de829017f03137d863fe0bf2b5e4
Christian Kaisermann <christian@kaisermann.me>
2016-11-29 14:16:13 -0200

    Splitted phrases to a different file

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
+const fs = require('fs');
 const TwitterPackage = require('twitter');
-const secret = require("./secret.json");
+const secret = JSON.parse(fs.readFileSync("./secret.json"));
+const phrases = JSON.parse(fs.readFileSync("./phrases.json"));
 const Twitter = new TwitterPackage(secret);
-
-const phrases = [
-  "PUTA MERDA COM CERTEZA VAI DAR MERDA",
-  "Vai dar merda.",
-  "Não sei se vai dar merda não.",
-  "Porra, vai dar merda total.",
-  "Até que não vai dar merda não.",
-  "Acabei de ver aqui que vai dar merda sim.",
-  "Nhé, até que talvez nem dê merda.",
-  "Pensa direitinho se vai dar merda mesmo.",
-  "VISH MAINHA VAI DAR MERDA LEGAL.",
-  "Hello darkness my old friend... Vai dar merda.",
-  "Já deu merda e tu nem percebeu.",
-  "Minhas fontes dizem que não, não vai dar merda.",
-  "Sim - vai dar merda com certeza.",
-  "Pode dar merda, pode não dar, a gente nunca sabe.",
-  "Não estou nem sentindo o cheiro da merda.",
-  "Deu merda.",
-  "Talvez dê merda, talvez não.",
-  "Não vai dar merda.",
-  "Fica tranquilho que hoje não vai dar merda. Hoje.",
-  "Meeeerda merda talvez não, no máximo um peidinho."
-];
 
 Twitter.stream('statuses/filter', {
   track: '#VaiDarMerda'
